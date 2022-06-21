2f2567c1fbe027dc298d6e04fefa85b87e236754
sahanruwanthasilva <sahanr.silva@hotmail.com>
2022-06-21 19:07:50 +0530

    updated SecurityConstants

--- src/main/java/com/sahan/workerslobby/Constants/SecurityConstants.java
+++ src/main/java/com/sahan/workerslobby/Constants/SecurityConstants.java
@@ -1,4 +1,16 @@
 package com.sahan.workerslobby.Constants;
 
-public class SecurityConstants {
+public class SecurityConstants
+{
+    public static final long EXPIRATION_TIME = 432_000_00; // 5 days
+    public static final String TOKEN_HEADER = "Bearer ";
+    public static final String JWT_TOKEN_HEADER = "Jwt-Token";
+    public static final String TOKEN_CANNOT_BE_VERIFIED = "Token cannot be verified";
+    public static final String SAHAN_SYSTEMS = "Sahan systems and solutions";
+    public static final String SAHAN_SYSTEMS_ADMINISTRATION = "workers lobby";
+    public static final String AUTHORITIES = "Authorities";
+    public static final String FORBIDDEN_MESSAGE = "you need to log in to access this page";
+    public static final String ACCESS_DENIED_MESSAGE = "You do not have permission to access this page";
+    public static final String OPTIONS_HTTP_METHOD = "OPTIONS";
+    public static final String[] PUBLIC_URLS = {"/user/login", "/user/register", "/user/resetpassowrd/**", "/user/image/**"};
 }
